cade3938823dc430af780255d3bcd4cae77b552f
= <reser13@gmail.com>
2018-11-16 16:28:24 -0500

    REP-292 - Redirecting on error works when user fails to load.

--- src/components/users/userManagementForm.tsx
+++ src/components/users/userManagementForm.tsx
@@ -46,12 +46,10 @@ interface UserManagementProps {
 
 const UserManagementForm: React.SFC<UserManagementProps> = (props: UserManagementProps) => (
     <React.Fragment>
-    {props.managedUser != null ?
+        {props.redirectToErrorPage ? <Redirect to="/error" /> : null }
+        {props.managedUser != null ?
         <div className="management-container">
-        {props.redirectToErrorPage ?
-            <Redirect to="/error" /> 
-        : null }
-            {props.activePanelIndex != null ? <EditablePanelOverlay /> : null}
+            {props.activePanelIndex != null ? <Overlay /> : null}
             <div className="management-left">
                 <UserManagementProfile top={true} initialValues={props.managedUser} />
                 <EditablePanel  active={props.activePanelIndex === 0}

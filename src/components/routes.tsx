reperio/platform-ui

REP-245 - Added permissions to user model, added corePermissions to permissions model. Container routes now have required permissions in order to load. Panel accepts prop 'permissionToEdit' to disabled hand cursor and onCLick action. All management pages and navbar routes have permission checking.

src/components/routes.tsx
@@ -22,24 +22,23 @@ import OrganizationCreateFormContainer from "../containers/organizations/organiz
 import ForgotPasswordFormContainer from "../containers/auth/forgotPasswordFormContainer";
 import ResetPasswordFormContainer from "../containers/auth/resetPasswordFormContainer";
 import ErrorContainer from '../containers/errorContainer';
+import { CorePermissions } from '../models/permission';
 
 const Routes = () => (
     <div className="app-content">
         <Switch>
             <PrivateRouteContainer exact path="/home" component={MainDashboardContainer} />
-            <PrivateRouteContainer exact path="/users" component={UsersContainer} />
-            <PrivateRouteContainer exact path="/users/new" component={UserCreateFormContainer} />
-            <PrivateRouteContainer exact path="/users/:userId/edit" component={UserManagementFormContainer} />
-            <PrivateRouteContainer exact path="/userCreate" component={UserCreateFormContainer} />
-            <PrivateRouteContainer exact path="/userManagement" component={UserManagementFormContainer} />
-            <PrivateRouteContainer exact path="/roles" component={RolesContainer} />
-            <PrivateRouteContainer exact path="/roles/:roleId/edit" component={RoleManagementContainer} />
-            <PrivateRouteContainer exact path="/roles/new" component={RoleCreateFormContainer} />
-            <PrivateRouteContainer exact path="/permissions" component={PermissionsContainer} />
-            <PrivateRouteContainer exact path="/permissions/:permissionName/edit" component={PermissionManagementContainer} />
-            <PrivateRouteContainer exact path="/organizations" component={OrganizationsContainer} />
-            <PrivateRouteContainer exact path="/organizations/:organizationId/edit" component={OrganizationManagementContainer} />
-            <PrivateRouteContainer exact path="/organizations/new" component={OrganizationCreateFormContainer} />
+            <PrivateRouteContainer requiredPermissions={[CorePermissions.ViewUsers]} exact path="/users" component={UsersContainer} />
+            <PrivateRouteContainer requiredPermissions={[CorePermissions.ViewUsers, CorePermissions.CreateUsers]} exact path="/users/new" component={UserCreateFormContainer} />
+            <PrivateRouteContainer requiredPermissions={[CorePermissions.ViewUsers]} exact path="/users/:userId/edit" component={UserManagementFormContainer} />
+            <PrivateRouteContainer requiredPermissions={[CorePermissions.ViewRoles]} exact path="/roles" component={RolesContainer} />
+            <PrivateRouteContainer requiredPermissions={[CorePermissions.ViewRoles]} exact path="/roles/:roleId/edit" component={RoleManagementContainer} />
+            <PrivateRouteContainer requiredPermissions={[CorePermissions.ViewRoles, CorePermissions.CreateRoles]} exact path="/roles/new" component={RoleCreateFormContainer} />
+            <PrivateRouteContainer requiredPermissions={[CorePermissions.ViewPermissions]} exact path="/permissions" component={PermissionsContainer} />
+            <PrivateRouteContainer requiredPermissions={[CorePermissions.ViewPermissions]} exact path="/permissions/:permissionName/edit" component={PermissionManagementContainer} />
+            <PrivateRouteContainer requiredPermissions={[CorePermissions.ViewOrganizations]} exact path="/organizations" component={OrganizationsContainer} />
+            <PrivateRouteContainer requiredPermissions={[CorePermissions.ViewOrganizations]} exact path="/organizations/:organizationId/edit" component={OrganizationManagementContainer} />
+            <PrivateRouteContainer requiredPermissions={[CorePermissions.ViewOrganizations, CorePermissions.CreateOrganizations]} exact path="/organizations/new" component={OrganizationCreateFormContainer} />
             <PublicRouteContainer exact path="/login" component={LoginFormContainer} />
             <PublicRouteContainer exact path="/signup" component={SignupFormContainer} />
             <PublicRouteContainer exact path="/emailVerification/:token" component={EmailVerificationContainer} />

reperio/core-ui

src/components/routes.tsx
@@ -16,24 +16,23 @@ import OrganizationsContainer from "../containers/organizations/organizationsCon
 import OrganizationManagementContainer from "../containers/organizations/organizationManagementContainer";
 import OrganizationCreateFormContainer from "../containers/organizations/organizationCreateFormContainer";
 import ErrorContainer from '../containers/errorContainer';
+import { CorePermissions } from '../models/permission';
 
 const Routes = () => (
     <div className="app-content">
         <Switch>
             <RouteContainer exact path="/home" component={MainDashboardContainer} />
-            <RouteContainer exact path="/users" component={UsersContainer} />
-            <RouteContainer exact path="/users/new" component={UserCreateFormContainer} />
-            <RouteContainer exact path="/users/:userId/edit" component={UserManagementFormContainer} />
-            <RouteContainer exact path="/userCreate" component={UserCreateFormContainer} />
-            <RouteContainer exact path="/userManagement" component={UserManagementFormContainer} />
-            <RouteContainer exact path="/roles" component={RolesContainer} />
-            <RouteContainer exact path="/roles/:roleId/edit" component={RoleManagementContainer} />
-            <RouteContainer exact path="/roles/new" component={RoleCreateFormContainer} />
-            <RouteContainer exact path="/permissions" component={PermissionsContainer} />
-            <RouteContainer exact path="/permissions/:permissionName/edit" component={PermissionManagementContainer} />
-            <RouteContainer exact path="/organizations" component={OrganizationsContainer} />
-            <RouteContainer exact path="/organizations/:organizationId/edit" component={OrganizationManagementContainer} />
-            <RouteContainer exact path="/organizations/new" component={OrganizationCreateFormContainer} />
+            <RouteContainer requiredPermissions={[CorePermissions.ViewUsers]} exact path="/users" component={UsersContainer} />
+            <RouteContainer requiredPermissions={[CorePermissions.ViewUsers, CorePermissions.CreateUsers]} exact path="/users/new" component={UserCreateFormContainer} />
+            <RouteContainer requiredPermissions={[CorePermissions.ViewUsers]} exact path="/users/:userId/edit" component={UserManagementFormContainer} />
+            <RouteContainer requiredPermissions={[CorePermissions.ViewRoles]} exact path="/roles" component={RolesContainer} />
+            <RouteContainer requiredPermissions={[CorePermissions.ViewRoles]} exact path="/roles/:roleId/edit" component={RoleManagementContainer} />
+            <RouteContainer requiredPermissions={[CorePermissions.ViewRoles, CorePermissions.CreateRoles]} exact path="/roles/new" component={RoleCreateFormContainer} />
+            <RouteContainer requiredPermissions={[CorePermissions.ViewPermissions]} exact path="/permissions" component={PermissionsContainer} />
+            <RouteContainer requiredPermissions={[CorePermissions.ViewPermissions]} exact path="/permissions/:permissionName/edit" component={PermissionManagementContainer} />
+            <RouteContainer requiredPermissions={[CorePermissions.ViewOrganizations]} exact path="/organizations" component={OrganizationsContainer} />
+            <RouteContainer requiredPermissions={[CorePermissions.ViewOrganizations]} exact path="/organizations/:organizationId/edit" component={OrganizationManagementContainer} />
+            <RouteContainer requiredPermissions={[CorePermissions.ViewOrganizations, CorePermissions.CreateOrganizations]} exact path="/organizations/new" component={OrganizationCreateFormContainer} />
             <RouteContainer exact path="/error" component={ErrorContainer} />
             <Route>
                 <Redirect to="/home"/>
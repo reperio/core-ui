bcba3885e9313ef72beefd6c9737213b896c6490
= <reser13@gmail.com>
2018-12-14 10:53:42 -0500

    REP-266 - Added organization component tests.

---
+++ src/components/organizations/tests/organizationFieldArray.spec.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import { mount } from 'enzyme';
+import OrganizationFieldArray from '../organizationFieldArray';
+import { ButtonElement } from '@reperio/ui-components';
+import { Organization } from '@reperio/core-connector';
+
+it('Checks to see clicking delete button calls removeOrganization with 1 param', () => {
+    const initialValues: Organization[] = [
+        {
+            id: "966f4157-934c-45e7-9f44-b1e5fd8b79a7",
+            name: "Test Organization",
+            personal: true,
+            userOrganizations: [
+                {
+                    organization: {id: "966f4157-934c-45e7-9f44-b1e5fd8b79a7", name: "Test Organization", personal: true, userOrganizations: []},
+                    organizationId: "966f4157-934c-45e7-9f44-b1e5fd8b79a7",
+                    user: null,
+                    userId: "d08a1f76-7c4a-4dd9-a377-83ffffa752f4"
+                },
+                {
+                    organization: {id: "fbcb4a99-1fe0-4fec-b5f7-64efbeeb1893", name: "bbbb", personal: false, userOrganizations: []},
+                    organizationId: "fbcb4a99-1fe0-4fec-b5f7-64efbeeb1893",
+                    user: null,
+                    userId: "d08a1f76-7c4a-4dd9-a377-83ffffa752f4"
+                }
+            ]
+        }
+    ];
+
+    const mockDelete = jest.fn();
+    const wrapper = mount(
+        <OrganizationFieldArray removeOrganization={mockDelete} 
+                                initialValues={initialValues} 
+                                active={true} />
+                    );
+
+    wrapper.find(ButtonElement).at(0).simulate('click');
+    expect(mockDelete).toHaveBeenCalledWith(0);
+
+});
---
+++ src/components/organizations/tests/organizations.spec.tsx
@@ -0,0 +1,56 @@
+import * as React from 'react';
+import { mount } from 'enzyme';
+import Organizations from '../organizations';
+import { Organization } from '@reperio/core-connector';
+
+const orgData : Organization[] = [
+    {
+        id: "966f4157-934c-45e7-9f44-b1e5fd8b79a7",
+        name: "Test Organization",
+        personal: true,
+        userOrganizations: [
+            {
+                userId: '',
+                organizationId: '',
+                user:  null,
+                organization: null
+            }
+        ]
+    }
+];
+
+it('Checks to see if organization create button calls a function', () => {
+    const mockCreate = jest.fn();
+    const wrapper = mount(
+        <Organizations  navigateToManagement={() => {}} 
+                        navigateToCreate={mockCreate} 
+                        gridData={[]} />
+                    );
+
+    wrapper.find('button').at(0).simulate('click');
+    expect(mockCreate).toHaveBeenCalled();
+});
+
+it('Checks if grid has data in it', () => {
+
+
+    const wrapper = mount(
+        <Organizations  navigateToManagement={() => {}} 
+                        navigateToCreate={() => {}} 
+                        gridData={orgData} />
+                    );
+
+    expect(wrapper.find('.rt-td').first().text()).toEqual(orgData[0].name);
+});
+
+it('Check if clicking row calls function', () => {
+    const mockManage = jest.fn();
+    const wrapper = mount(
+        <Organizations  navigateToManagement={mockManage} 
+                        navigateToCreate={() => {}} 
+                        gridData={orgData} />
+                    );
+
+    wrapper.find('.rt-td').first().simulate('click');
+    expect(mockManage).toHaveBeenCalled();
+});

6b212d2770262db3025562006cd1e3f7a855e7cf
Alec Reser <reser13@gmail.com>
2019-09-18 14:16:43 -0400

    Added query capabilities to roles grid.

--- src/containers/roles/rolesContainer.tsx
+++ src/containers/roles/rolesContainer.tsx
@@ -2,20 +2,17 @@ import React from 'react'
 import {connect} from "react-redux";
 import {bindActionCreators} from "redux";
 import Roles from "../../components/roles/roles";
-import { getRoles } from '../../actions/rolesActions';
+import { getRolesQuery } from '../../actions/rolesActions';
 import { State } from '../../store/initialState';
 import { history } from '../../store/history';
 import { RouteComponentProps } from 'react-router';
+import { Sort, Filter, QueryParameters } from '@reperio/core-connector';
 
 interface StateProps extends ReturnType<typeof mapStateToProps> {}
 
 interface DispatchProps extends ReturnType<typeof mapActionToProps> {}
 
 class RolesContainer extends React.Component<RouteComponentProps<any> & StateProps & DispatchProps> {
-
-    async componentDidMount() {
-        await this.props.actions.getRoles();
-    }
 
     navigateToCreate() {
         history.push('/roles/new');
@@ -25,24 +22,35 @@ class RolesContainer extends React.Component<RouteComponentProps<any> & StatePro
         history.push(`/roles/${roleId}/edit`);
     }
 
+    async queryData(page: number, pageSize: number, sort: Sort[], filter: Filter[]) {
+        const query: QueryParameters = ({
+            page, pageSize, sort, filter
+        });
+        await this.props.actions.getRolesQuery(query);
+        return this.props.rolesQuery.data;
+    }
+
     render() {
         return (
             <Roles  navigateToManagement={this.navigateToManagement.bind(this)} 
                     navigateToCreate={this.navigateToCreate.bind(this)} 
-                    gridData={this.props.roles.roles} />
+                    gridData={this.props.roles.roles}
+                    onFetchData={this.queryData.bind(this)}
+                    pages={this.props.rolesQuery.pages}  />
         );
     }
 }
 
 function mapStateToProps(state: State) {
     return {
-        roles: state.roles
+        roles: state.roles,
+        rolesQuery: state.queryResult
     };
 }
 
 function mapActionToProps(dispatch: any) {
     return {
-        actions: bindActionCreators({getRoles}, dispatch)
+        actions: bindActionCreators({getRolesQuery}, dispatch)
     };
 }
 
--- src/reducers/queryResultReducer.ts
+++ src/reducers/queryResultReducer.ts
@@ -1,5 +1,6 @@
 import {initialState, StateQueryResult} from "../store/initialState";
 import { usersActionTypes } from "../actions/usersActions";
+import { rolesActionTypes } from "../actions/rolesActions";
 
 export function queryResultReducer(state = initialState.queryResult, action: {type: string, payload: any}): StateQueryResult {
     switch (action.type) {
@@ -16,6 +17,24 @@ export function queryResultReducer(state = initialState.queryResult, action: {ty
             };
         }
         case usersActionTypes.USERS_QUERY_GET_ERROR: {
+            return {
+                data: [],
+                pages: 0
+            };
+        }
+        case rolesActionTypes.ROLES_QUERY_GET_PENDING: {
+            return {
+                data: state.data,
+                pages: state.pages
+            };
+        }
+        case rolesActionTypes.ROLES_QUERY_GET_SUCCESS: {
+            return {
+                data: action.payload.data,
+                pages: action.payload.pages
+            };
+        }
+        case rolesActionTypes.ROLES_QUERY_GET_ERROR: {
             return {
                 data: [],
                 pages: 0
